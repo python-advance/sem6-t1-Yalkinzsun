8121739304a91a8f0abd55b4bb5d78da54f87bed
Kirill Skorobogatov <Kirill9806@gmail.com>
2019-06-25 22:31:52 +0300

    Update package_check.py

--- Самостоятельная работа/Инвариантные задания/1.2/package_check.py
+++ Самостоятельная работа/Инвариантные задания/1.2/package_check.py
@@ -1,11 +1,11 @@
 from guestbookpackage import GuestBook
 
-if __name__ == "__main__":
-    guest_book = GuestBook()
-    guest_book.add_guest("Venom", "Dreadful", "venom@space.com")
-    guest_book.add_guest("Harold", "Great", "Harold@gmail.com")
-    guest_book.remove_guest("Harold@gmail.com")
-    guest_book.full_remove_guest("Ribo@gmail.com")
-    guest_book.rename_guest("Harold@gmail.com","REY12","Scalator")
-    guest_book.write_file()
-    guest_book.file_cleanup()
+
+guest_book = GuestBook()
+guest_book.add_guest("Venom", "Dreadful", "venom@space.com")
+guest_book.add_guest("Harold", "Great", "Harold@gmail.com")
+guest_book.remove_guest("Harold@gmail.com")
+guest_book.full_remove_guest("Ribo@gmail.com")
+guest_book.rename_guest("Harold@gmail.com","REY12","Scalator")
+guest_book.write_file()
+guest_book.file_cleanup()
